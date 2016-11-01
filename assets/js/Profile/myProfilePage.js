0a4a83457a8325f2eb72633c8e421d195f71ac43
WhileOfOne <mughesh@whileofone.com>
2016-11-01 18:12:28 +0530

    updated for adminLogo image url check for myProfilePgae.-saran

--- assets/js/Profile/myProfilePage.js
+++ assets/js/Profile/myProfilePage.js
@@ -943,7 +943,7 @@
                  $("#inputEmergencyElectricityNumber").css("padding", "10px 10px 10px 32px");
                  $(".adminEmerno-prefix").show();
                  $(".emerElectno-prefix").show();
-                 if (result.records[getUserInfo].Logo == null || result.records[getUserInfo].Logo == "") {
+                 if (result.records[getUserInfo].Logo == null || result.records[getUserInfo].Logo == "" || result.records[getUserInfo].Logo=="Fail upload folder with read access.") {
                      $("#adminLogoImage").show();
                  } else {
 
