495f608657806a8137bd7bd4ed9c932e7bf22502
jaya <jayashree.bopinder@whileofone.com>
2017-01-23 11:17:01 +0530

    App url changed-visali

--- assets/js/appUrl.js
+++ assets/js/appUrl.js
@@ -1,3 +1,3 @@
 /* exported   domainAddress */
-var domainAddress = "https://devapi.myrequest.co.uk/";
+var domainAddress = "https://dev.api.myrequest.co.uk/";
 //var domainAddress = "http://localhost:8888/myrequestapi/";
