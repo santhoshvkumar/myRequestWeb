0d8281f56f367a155c4a3c089fb2b7f9d8e14c9f
unknown <zulfikar.ayub@whileofone.com>
2017-01-28 15:45:40 +0530

    indentation done-zulfi

--- assets/js/ListTenant/listTenants.js
+++ assets/js/ListTenant/listTenants.js
@@ -6,9 +6,9 @@ $(function() {
         $window.resize();
     })
     var $switcher = $('#style_switcher'),
-        $switcher_toggle = $('#style_switcher_toggle'),
-        $theme_switcher = $('#theme_switcher'),
-        $mini_sidebar_toggle = $('#style_sidebar_mini');
+    $switcher_toggle = $('#style_switcher_toggle'),
+    $theme_switcher = $('#theme_switcher'),
+    $mini_sidebar_toggle = $('#style_sidebar_mini');
 
     $switcher_toggle.click(function(e) {
         e.preventDefault();
@@ -18,13 +18,13 @@ $(function() {
     $theme_switcher.children('li').click(function(e) {
         e.preventDefault();
         var $this = $(this),
-            this_theme = $this.attr('data-app-theme');
+        this_theme = $this.attr('data-app-theme');
 
         $theme_switcher.children('li').removeClass('active_theme');
         $(this).addClass('active_theme');
         $('body')
-            .removeClass('app_theme_a app_theme_b app_theme_c app_theme_d app_theme_e app_theme_f app_theme_g')
-            .addClass(this_theme);
+        .removeClass('app_theme_a app_theme_b app_theme_c app_theme_d app_theme_e app_theme_f app_theme_g')
+        .addClass(this_theme);
 
         if (this_theme == '') {
             localStorage.removeItem('altair_theme');
@@ -42,27 +42,27 @@ $(function() {
     // toggle mini sidebar
 
     $mini_sidebar_toggle
-        .on('ifChecked', function(event) {
-            $switcher.removeClass('switcher_active');
-            localStorage.setItem("altair_sidebar_mini", '1');
-            location.reload(true);
-        })
-        .on('ifUnchecked', function(event) {
-            $switcher.removeClass('switcher_active');
-            localStorage.removeItem('altair_sidebar_mini');
-            location.reload(true);
-        });
+    .on('ifChecked', function(event) {
+        $switcher.removeClass('switcher_active');
+        localStorage.setItem("altair_sidebar_mini", '1');
+        location.reload(true);
+    })
+    .on('ifUnchecked', function(event) {
+        $switcher.removeClass('switcher_active');
+        localStorage.removeItem('altair_sidebar_mini');
+        location.reload(true);
+    });
 
     // hide style switcher
     $document.on('click keyup', function(e) {
         if ($switcher.hasClass('switcher_active')) {
             if (
                 (!$(e.target).closest($switcher).length) || (e.keyCode == 27)
-            ) {
+                ) {
                 $switcher.removeClass('switcher_active');
-            }
         }
-    });
+    }
+});
 
     if (localStorage.getItem("altair_theme") !== null) {
         $theme_switcher.children('li[data-app-theme=' + localStorage.getItem("altair_theme") + ']').click();
@@ -124,8 +124,8 @@ $(document).ready(function() {
         getDateDiff(adminUserID);
         if(logo==undefined || logo==null || logo=="undefined" || logo=="Fail upload folder with read access."){
             $(".myRequestAdminLogo").attr("src", "assets/img/myRequestLogo.png").show();
-         }
-         else{
+        }
+        else{
             $(".myRequestAdminLogo").attr("src", domainAddress + logo).show();
             var getLogoImagePath = logo.slice(0,4);
             if(getLogoImagePath=="api/"){
@@ -135,7 +135,7 @@ $(document).ready(function() {
             else{
                 $(".myRequestAdminLogo").attr("src", domainAddress + logo).show();
             }
-         }
+        }
     }
 
 
@@ -171,14 +171,14 @@ $(document).ready(function() {
             $("#caseProperty").append("<option value='0'>No Property Found</option>");
         }
         else{
-           for (var Property in result.records) {
-                $("#caseProperty").append("<option value='" + result.records[Property].PropertyRegister + "' ref='" + result.records[Property].PropAddress + "'>" + result.records[Property].PropOwnerName + " - " + result.records[Property].PropOwnerEmail + " - " + result.records[Property].PropOwnerPhone + " - " + result.records[Property].PropAddress + "</option>");
-            } 
-        }
-        
+         for (var Property in result.records) {
+            $("#caseProperty").append("<option value='" + result.records[Property].PropertyRegister + "' ref='" + result.records[Property].PropAddress + "'>" + result.records[Property].PropOwnerName + " - " + result.records[Property].PropOwnerEmail + " - " + result.records[Property].PropOwnerPhone + " - " + result.records[Property].PropAddress + "</option>");
+        } 
+    }
 
 
-        $("#caseProperty").select2();
+
+    $("#caseProperty").select2();
 
 
     }); // GetAllPropertyList
@@ -189,27 +189,27 @@ $(document).ready(function() {
 
 }); // ready
 
- $("#inputMobileNumber").on('blur', function(e) {
-        var getMobileNumber = $("#" + this.id).val();
-       console.log("this is blur event");
-        $.get(domainAddress + "GetUserDetailsValue/" + getMobileNumber, function(result) {
-            console.log(result);
-            if (result.record_count == 0) {} else {
-                for (var getUserDetails in result.records) {
-                    $("#hiddenTenantID").val(result.records[getUserDetails].UserRegID);
-                    $("#getName").val(result.records[getUserDetails].Name);
-                    $("#getLastName").val(result.records[getUserDetails].LastName);
-                    $("#inputTitle").val(result.records[getUserDetails].Title);
-                    $("#select2-inputTitle-container").html(result.records[getUserDetails].Title);
-                    $("#inputEmailID").val(result.records[getUserDetails].EmailID);
-                   $(".md-input-wrapper").addClass("md-input-filled");
+$("#inputMobileNumber").on('blur', function(e) {
+    var getMobileNumber = $("#" + this.id).val();
+    console.log("this is blur event");
+    $.get(domainAddress + "GetUserDetailsValue/" + getMobileNumber, function(result) {
+        console.log(result);
+        if (result.record_count == 0) {} else {
+            for (var getUserDetails in result.records) {
+                $("#hiddenTenantID").val(result.records[getUserDetails].UserRegID);
+                $("#getName").val(result.records[getUserDetails].Name);
+                $("#getLastName").val(result.records[getUserDetails].LastName);
+                $("#inputTitle").val(result.records[getUserDetails].Title);
+                $("#select2-inputTitle-container").html(result.records[getUserDetails].Title);
+                $("#inputEmailID").val(result.records[getUserDetails].EmailID);
+                $(".md-input-wrapper").addClass("md-input-filled");
 
-                }
             }
+        }
 
 
-        });
     });
+});
 
 $(".btnSearch").click(function() {
     getValue = $("#inputSearch").val();
@@ -336,7 +336,7 @@ $(".btnSubmitTenantProperty").click(function() {
 
 
 $(".btnSubmitTenantInsurance").click(function() {
-    
+
     console.log("btnSubmitTenantInsurance click");
     var hiddenTenantID = $("#hiddenTenantID").val();
     propertyId = $("#caseProperty").val();
@@ -515,7 +515,7 @@ $(".getTenant").click(function() {
     $("#inputTitle").val(0);
     $("#getName").val('');
     $("#getLastName").val('');
-    $("#inputMobileNumber").val('');
+    $("#inputMobileNumber").val(''); 
     $("#inputEmailID").val('');
     $("#hiddenIsLeadTenant").val(0);
     $('.leadTenant > span').css('box-shadow', 'rgba(0, 0, 0, 0.258824) 0px 0px 0px 0px inset').css('border-color', 'rgba(0, 0, 0, 0.258824)').css('transition', 'border 0.4s, box-shadow 0.4s').css('background-color', 'rgba(0, 0, 0, 0.258824)');
@@ -649,15 +649,15 @@ function getTenantsList(getValue) {
         url: sendURL,
         data: dataForm,
         success: function(result) {
-                console.log(result);
+            console.log(result);
 
-                if (result.record_count == 0 && result.All_Records_Count == 0) {
-                    $(".allTenantList").html('');
-                    $(".allTenantList").append("<tr id='rowID-0'><td id='name-0'>No Records Found</td> <td id='phoneNumber-0'>  </td><td id='emailID-0'> </td>  <td> </td> <td> </td></tr> ");
-                    $("#getLoadingModalContent").removeClass('md-show');
-                } else {
-                    loadUserTenantsList(result);
-                }
+            if (result.record_count == 0 && result.All_Records_Count == 0) {
+                $(".allTenantList").html('');
+                $(".allTenantList").append("<tr id='rowID-0'><td id='name-0'>No Records Found</td> <td id='phoneNumber-0'>  </td><td id='emailID-0'> </td>  <td> </td> <td> </td></tr> ");
+                $("#getLoadingModalContent").removeClass('md-show');
+            } else {
+                loadUserTenantsList(result);
+            }
             } // ajax success
     }); // ajax POSTS
 }
@@ -768,23 +768,23 @@ function loadUserTenantsList(result) {
         }); // editDepartment
 
 
-        $(".deleteTenant").on('click', function(e) {
-            var deleteTenantID = this.id.replace('deleteTenantID-', '');
+$(".deleteTenant").on('click', function(e) {
+    var deleteTenantID = this.id.replace('deleteTenantID-', '');
 
-            UIkit.modal.confirm('Are you sure?', function() {
-                $("#getLoadingModalContent").addClass('md-show');
-                $.post(domainAddress + 'DeleteUserTenant/' + deleteTenantID + "/" + adminUserID, function(e) {
-                    console.log(e);
-                    $("#getLoadingModalContent").removeClass('md-show');
-                    $("#rowID-" + deleteTenantID).remove();
-                    getTenantsList(getValue);
-                    UIkit.modal.alert('Tenant Deleted Successfully');
-                });
-            });
+    UIkit.modal.confirm('Are you sure?', function() {
+        $("#getLoadingModalContent").addClass('md-show');
+        $.post(domainAddress + 'DeleteUserTenant/' + deleteTenantID + "/" + adminUserID, function(e) {
+            console.log(e);
+            $("#getLoadingModalContent").removeClass('md-show');
+            $("#rowID-" + deleteTenantID).remove();
+            getTenantsList(getValue);
+            UIkit.modal.alert('Tenant Deleted Successfully');
+        });
+    });
 
         }); // deleteTenant
 
-    }
+}
 
 } // loadUserTenantsList
 
@@ -831,8 +831,8 @@ function getTenantPropertyList(hiddenTenantID) {
                 });
 
             });
-        }
-    });
+}
+});
 }
 
 
@@ -1051,7 +1051,7 @@ $(".btnSubmitTenant").click(function() {
     var startDate = $("#inputStartDate").val();
     var getFormatStartDate = startDate.split(".");
     var finalStartDate = getFormatStartDate[2] + "-" + getFormatStartDate[1] + "-" + getFormatStartDate[0];
-   
+
     var endDate = $("#inputEndDate").val();
     var getFormatEndDate = endDate.split(".");
     var finalEndDate = getFormatEndDate[2] + "-" + getFormatEndDate[1] + "-" + getFormatEndDate[0];
@@ -1126,22 +1126,22 @@ $(".btnSubmitTenant").click(function() {
             return false;
         }
         if (startDate == "") {
-        $(".help-block").css("border-color", "red");
-        $(".help-block").show();
-        $(".help-block").text("* Enter the Start Date");
-        $("#inputStartDate").css("border-color", "red");
-        $(".btnSubmitTenant").attr("disabled", true);
-        return false;
-    }
+            $(".help-block").css("border-color", "red");
+            $(".help-block").show();
+            $(".help-block").text("* Enter the Start Date");
+            $("#inputStartDate").css("border-color", "red");
+            $(".btnSubmitTenant").attr("disabled", true);
+            return false;
+        }
 
-    if (endDate == "") {
-        $(".help-block").css("border-color", "red");
-        $(".help-block").show();
-        $(".help-block").text("* Enter the End Date");
-        $("#inputEndDate").css("border-color", "red");
-        $(".btnSubmitTenant").attr("disabled", true);
-        return false;
-    } 
+        if (endDate == "") {
+            $(".help-block").css("border-color", "red");
+            $(".help-block").show();
+            $(".help-block").text("* Enter the End Date");
+            $("#inputEndDate").css("border-color", "red");
+            $(".btnSubmitTenant").attr("disabled", true);
+            return false;
+        } 
         else {
             var propertyAddress = $("#caseProperty :selected").attr("ref");
             dataAddPropertyForm = "{'Property_RegisterID':'" + propertyId + "','AdminID':'" + adminUserID + "','PropertyAddress':'"+propertyAddress+"','IsElectricity':'"+hiddenIsElectricity+"','IsGas':'"+hiddenIsGas+"','IsWater':'"+hiddenIsWater+"','IsCouncil':'"+hiddenIsCouncil+"','IsAvailTenantInsurance':'"+hiddenAvailTenantInsurance+"'}";
