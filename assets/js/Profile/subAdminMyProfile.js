1ac898c308a33dfbf6c1ac405b56faaea176a4e4
sarath <sarath@whileofone.com>
2017-01-30 20:15:05 +0530

    revert code-mukesh

--- assets/js/Profile/subAdminMyProfile.js
+++ assets/js/Profile/subAdminMyProfile.js
@@ -297,7 +297,6 @@
          window.location.href = "index.html";
      } else {
          $(".getUserName").text(adminUserName);
-         $("#FileURLUploadImage2").attr("action",domainAddress+"ajaximage.php");
      }
 
 
