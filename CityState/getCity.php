ee5f5c4c6184018fad8c6fdfafde21638b339e0c
WhileOfOne <mughesh@whileofone.com>
2017-02-02 10:22:05 +0530

    changed for City-mukesh

--- CityState/getCity.php
+++ CityState/getCity.php
@@ -5,27 +5,27 @@ $getStateID = $_GET["stateID"];
 $row = 0;
 
 if (($handle = fopen("city.csv", "r")) !== FALSE) {
-    while (($data = fgetcsv($handle, 0, ",")) !== FALSE) {
-        $num = count($data);
+	while (($data = fgetcsv($handle, 0, ",")) !== FALSE) {
+		$num = count($data);
 
-        if($data[1] == $getStateID){
-            $resultArr[$row]['CityName'] = $data[2];
+		if($data[1] == $getStateID){
+			$resultArr[$row]['CityName'] = $data[2];
 
-         $row++;
-          
-        }
-         
+			$row++;
+			
+		}
+		
 
-   			
-   	}
+		
+	}
 
 }
 
 echo json_encode(array("status"=>"success","record_count"=>$row,"records"=>$resultArr),JSON_UNESCAPED_SLASHES);
 
 
-   
-    fclose($handle);
+
+fclose($handle);
 
 
 ?>
--- CityState/getState.php
+++ CityState/getState.php
@@ -5,27 +5,27 @@ $getCountryId = $_GET["countryID"];
 $row = 0;
 
 if (($handle = fopen("state.csv", "r")) !== FALSE) {
-    while (($data = fgetcsv($handle, 0, ",")) !== FALSE) {
-        $num = count($data);
+	while (($data = fgetcsv($handle, 0, ",")) !== FALSE) {
+		$num = count($data);
 
-        if($data[1] == $getCountryId){
-            $resultArr[$row]['StateName'] = $data[2];
+		if($data[1] == $getCountryId){
+			$resultArr[$row]['StateName'] = $data[2];
 
-         $row++;
-          
-        }
-         
+			$row++;
+			
+		}
+		
 
-        
-    }
+		
+	}
 
 }
 
 echo json_encode(array("status"=>"success","record_count"=>$row,"records"=>$resultArr),JSON_UNESCAPED_SLASHES);
 
 
-   
-    fclose($handle);
+
+fclose($handle);
 
 
 ?>
