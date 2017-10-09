a134dea2c4bbbde4f4e2c0a24a35fc6d7c5c29db
WhileOfOne Dev <whileofonedev@WhileOfOnes-Mac-mini.local>
2017-10-09 20:52:30 +0530

    Background image issue fixed for superadmin-Navaneeth

--- assets/js/appUrl.js
+++ assets/js/appUrl.js
@@ -8,6 +8,11 @@ var domainAddress = "https://api.myrequest.co.uk/";
 
 var getCountry = localStorage.getItem("MyRequest_countryCode");
 
+var adminType = localStorage.getItem("MyRequest_AdminType");
+    if(adminType == "SuperAdmin"){
+        $("body").addClass("bgUKImage");
+    }
+
     if(getCountry == "UK"){
         $("body").addClass("bgUKImage");
     } else if(getCountry == "US"){
